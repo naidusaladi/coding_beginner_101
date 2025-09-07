4390faaadea209f4c771004a268e95806433ae3b
mohanreddykarri1-dot <mohanreddykarri1@gmail.com>
2025-09-07 12:22:56 +0530

    fourth question is solved without no worries

---
+++ answer04.py
@@ -0,0 +1,9 @@
+a = int(input("enter first marks "))
+b = int(input("enter second marks "))
+c = int(input("enter third marks "))
+d = int(input("enter fourth marks "))
+condition = (a+b+c+d)/4
+if condition > 75 :
+    print("Topper")
+else:
+    print("Not a Topper")
